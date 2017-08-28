7eb2769ebc5ea66c828b1abd3665176c134f2b16
Max Kaplan <mekapl03@syr.edu>
2017-08-28 17:30:42 -0400

    adding eslint test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('transpile-module', () => {
   return gulp.src(['./src/index.js', './src/CurrencyMap.js'])
@@ -19,6 +20,12 @@ gulp.task('babel-module', () => {
 
 gulp.task('babel-classes', () => {
   return gulp.watch(['./src/classes/*.js'], ['transpile-classes']);
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['src/**/*.js','!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
 });
 
 gulp.task('default', [
