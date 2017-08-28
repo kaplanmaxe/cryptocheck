d88168747fd670983ff0908e1ef4031eebd94fbd
YoGeSh <yogesh.k@seventablets.com>
2017-08-28 12:14:13 -0500

    dist added.

--- dist/classes/Kraken.js
+++ dist/classes/Kraken.js
@@ -1,35 +1,12 @@
-'use strict';
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();var _request = require('request');var _request2 = _interopRequireDefault(_request);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}var
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _request = require('request');
-
-var _request2 = _interopRequireDefault(_request);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Kraken = function () {
-  function Kraken() {
-    _classCallCheck(this, Kraken);
-  }
-
-  _createClass(Kraken, null, [{
-    key: 'getCurrency',
-
+Kraken = function () {function Kraken() {_classCallCheck(this, Kraken);}_createClass(Kraken, null, [{ key: 'getCurrency',
     /**
-     * Gets currency pair price
-     *
-     * @param {string} asset Asset to check price of
-     */
-    value: function getCurrency(asset) {
-      var currency = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'USD';
-
+                                                                                                                           * Gets currency pair price
+                                                                                                                           *
+                                                                                                                           * @param {string} asset Asset to check price of
+                                                                                                                           */value: function getCurrency(
+    asset) {var currency = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'USD';
       return new Promise(function (resolve, reject) {
         (0, _request2.default)('https://api.kraken.com/0/public/Ticker?pair=' + asset + currency, function (err, response, body) {
           var output = JSON.parse(body);
@@ -38,10 +15,4 @@ var Kraken = function () {
           resolve(output.result[Object.keys(output.result)[0]].c[0]);
         });
       });
-    }
-  }]);
-
-  return Kraken;
-}();
-
-exports.default = Kraken;
+    } }]);return Kraken;}();exports.default = Kraken;
--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,20 @@
 #!/usr/bin/env node
 'use strict';
-
-var _commander = require('commander');
-
-var _commander2 = _interopRequireDefault(_commander);
-
-var _Helpers = require('./classes/Helpers');
-
-var _Helpers2 = _interopRequireDefault(_Helpers);
-
-var _GDAX = require('./classes/GDAX');
-
-var _GDAX2 = _interopRequireDefault(_GDAX);
-
-var _CoinMarketCap = require('./classes/CoinMarketCap');
-
-var _CoinMarketCap2 = _interopRequireDefault(_CoinMarketCap);
-
-var _Kraken = require('./classes/Kraken');
-
-var _Kraken2 = _interopRequireDefault(_Kraken);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _commander = require('commander');var _commander2 = _interopRequireDefault(_commander);
+var _Helpers = require('./classes/Helpers');var _Helpers2 = _interopRequireDefault(_Helpers);
+var _GDAX = require('./classes/GDAX');var _GDAX2 = _interopRequireDefault(_GDAX);
+var _CoinMarketCap = require('./classes/CoinMarketCap');var _CoinMarketCap2 = _interopRequireDefault(_CoinMarketCap);
+var _Kraken = require('./classes/Kraken');var _Kraken2 = _interopRequireDefault(_Kraken);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 _commander2.default.version('1.0.0');
 
 /**
- * Gets associated data for currency given symbol
- *
- * @param  {string} currency Symbol of currency
- * @param  {string} source gdax|kraken
- * @return {object}
- */
+                                       * Gets associated data for currency given symbol
+                                       *
+                                       * @param  {string} currency Symbol of currency
+                                       * @param  {string} source gdax|kraken
+                                       * @return {object}
+                                       */
 function getCurrencyData(currency, source) {
   var currencyData = _Helpers2.default.mapCurrency(currency);
   var index = source === 'gdax' ? 0 : currencyData.symbols.length - 1;
@@ -43,9 +26,12 @@ function getCurrencyData(currency, source) {
 }
 
 // GDAX
-_commander2.default.command('gdax [currency]').action(function (currency) {
+_commander2.default.
+command('gdax [currency]').
+action(function (currency) {
   var currencyData = getCurrencyData(currency, 'gdax');
-  _GDAX2.default.getCurrency(currencyData.symbol).then(function (res) {
+  _GDAX2.default.getCurrency(currencyData.symbol).
+  then(function (res) {
     console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res));
   }, function (err) {
     console.log(err);
@@ -53,8 +39,11 @@ _commander2.default.command('gdax [currency]').action(function (currency) {
 });
 
 // CoinMarketCap
-_commander2.default.command('cmc [currency]').action(function (currency) {
-  _CoinMarketCap2.default.getCurrency(currency).then(function (res) {
+_commander2.default.
+command('cmc [currency]').
+action(function (currency) {
+  _CoinMarketCap2.default.getCurrency(currency).
+  then(function (res) {
     console.log(res.name + ' (' + res.symbol + '): $' + _Helpers2.default.round(res.price_usd) + ' (' + res.percent_change_24h + '%)');
   }, function (err) {
     console.log(err);
@@ -62,13 +51,58 @@ _commander2.default.command('cmc [currency]').action(function (currency) {
 });
 
 // Kraken
-_commander2.default.command('kraken [currency]').action(function (currency) {
+_commander2.default.
+command('kraken [currency]').
+action(function (currency) {
   var currencyData = getCurrencyData(currency, 'kraken');
-  _Kraken2.default.getCurrency(currencyData.symbol).then(function (res) {
+  _Kraken2.default.getCurrency(currencyData.symbol).
+  then(function (res) {
     console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res));
   }, function (err) {
     console.log(err);
   });
 });
 
+
+
+_commander2.default.
+command('portfolio [fileLocation]').
+action(function (fileLocation) {
+  var fullPath = process.cwd() + '/' + fileLocation;
+  console.log('path :' + fullPath);
+  var fs = require('fs');
+  fs.readFile(fullPath, 'utf8', function (err, data) {
+    if (err) throw err; // we'll not consider error handling for now
+    var obj = JSON.parse(data);
+    var keys = Object.keys(obj);
+    var proms = [];
+    var data = {};
+    keys.forEach(function (key) {
+      data[key.toUpperCase()] = obj[key];
+      proms.push(_CoinMarketCap2.default.getCurrency(key));
+    }, this);
+    Promise.all(proms).
+    then(function (res) {
+      showPortfolio(res, data);
+    }, function (err) {
+      console.log('Errored ' + err);
+    });
+  });
+});
+
+function showPortfolio(res, obj) {
+  console.log('--------------------------------------------------------------------------------------------');
+  console.log('Symbol   Price     Acct. Val     %Change');
+  console.log('--------------------------------------------------------------------------------------------');
+  var total = 0;
+  res.forEach(function (currency) {
+
+    var currentVal = _Helpers2.default.round(obj[currency.symbol] * currency.price_usd);
+    total += Number(currentVal);
+    console.log(currency.symbol + '    $' + _Helpers2.default.round(currency.price_usd) + '    $' + currentVal + '      $' + currency.percent_change_24h + '%');
+  }, this);
+  console.log('--------------------------------------------------------------------------------------------');
+  console.log('Total : $' + _Helpers2.default.round(total));
+
+};
 _commander2.default.parse(process.argv);
