ce4223e2aa39f959a06a7661b7550326d6b250a9
Patrick Metcalfe <git@patrickmetcalfe.com>
2017-08-29 15:58:50 -0500

    add some tests

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,8 @@ require('console.table');
 var _Helpers = require('./classes/Helpers');var _Helpers2 = _interopRequireDefault(_Helpers);
 var _GDAX = require('./classes/GDAX');var _GDAX2 = _interopRequireDefault(_GDAX);
 var _CoinMarketCap = require('./classes/CoinMarketCap');var _CoinMarketCap2 = _interopRequireDefault(_CoinMarketCap);
-var _Kraken = require('./classes/Kraken');var _Kraken2 = _interopRequireDefault(_Kraken);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+var _Kraken = require('./classes/Kraken');var _Kraken2 = _interopRequireDefault(_Kraken);
+var _PortfolioResolver = require('./classes/PortfolioResolver');var _PortfolioResolver2 = _interopRequireDefault(_PortfolioResolver);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 _commander2.default.version('1.0.0');
 
@@ -34,7 +35,7 @@ action(function (currency) {
   var currencyData = getCurrencyData(currency, 'gdax');
   _GDAX2.default.getCurrency(currencyData.symbol).
   then(function (res) {
-    console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res));
+    console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res.price_usd));
   }, function (err) {
     console.log(err);
   });
@@ -59,7 +60,7 @@ action(function (currency) {
   var currencyData = getCurrencyData(currency, 'kraken');
   _Kraken2.default.getCurrency(currencyData.symbol).
   then(function (res) {
-    console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res));
+    console.log(currencyData.name + ' (' + currency.toUpperCase() + '): $' + _Helpers2.default.round(res.price_usd));
   }, function (err) {
     console.log(err);
   });
@@ -72,60 +73,30 @@ option('-m, --market [market]').
 action(function (fileLocation, options) {
   var fullPath = process.cwd() + '/' + fileLocation;
   var market = options.market || 'cmc';
+  var portfolioResolver = new _PortfolioResolver2.default({ market: market });
   _fs2.default.readFile(fullPath, 'utf8', function (err, data) {
     if (err) throw err;
     var obj = JSON.parse(data);
-    var keys = Object.keys(obj);
-    var proms = [];
-    var portfolioData = {};
-    keys.forEach(function (key) {
-      portfolioData[key.toUpperCase()] = obj[key];
-      proms.push(getPortfolioData(market.toLowerCase(), key));
-    }, this);
-    Promise.all(proms).
-    then(function (res) {
-      showPortfolio(res, portfolioData);
-    }, function (error) {
-      console.log('Errored ' + error);
+    portfolioResolver.getAssetValuesForPositions(obj).then(function (portfolio) {
+      var total = 0;
+      var formattedAssets = portfolio.assets.map(function (asset) {
+        total += asset.value;
+        asset.price = '$' + _Helpers2.default.round(asset.price);
+        asset.value = '$' + _Helpers2.default.round(asset.value);
+        asset.change = asset.change ? asset.change + '%' : "Unknown";
+        return asset;
+      });
+
+      console.table(formattedAssets);
+
+      if (portfolio.notSupported.length > 0) {
+        var formatedNotSupportedTickers = portfolio.notSupported.join(", ");
+        console.log('Market doesn\'t support: ' + formatedNotSupportedTickers);
+      }
+      console.log('Total: $' + _Helpers2.default.round(total));
+    }).catch(function (message) {
+      console.log('Errored: ' + message);
     });
   });
 });
-
-/**
-     * Returns appropriate promise as per market.
-     * @param {string} market
-     * @param {string} currency
-     */
-function getPortfolioData(market, currency) {
-  if (market === 'gdax') {
-    return _GDAX2.default.getCurrency(currency);
-  } else if (market === 'kraken') {
-    return _Kraken2.default.getCurrency(currency);
-  } else if (market === 'cmc') {
-    return _CoinMarketCap2.default.getCurrency(currency);
-  }
-
-  return null;
-}
-
-/**
-   * Prints portfolio
-   * @param {Array} res : array of dictionaries.
-   * @param {Dictionary} obj : portfolio dictinary.
-   */
-function showPortfolio(currencies, obj) {
-  var data = currencies.map(function (res) {
-    return {
-      symbol: res.symbol,
-      price: '$' + _Helpers2.default.round(res.price_usd),
-      value: '$' + _Helpers2.default.round(obj[res.symbol] * res.price_usd),
-      change: res.percent_change_24h + '%' };
-
-  });
-  var total = data.
-  map(function (res) {return Number(res.value.split('$')[1]);}).
-  reduce(function (sum, value) {return Number(sum) + value;}, 0);
-  console.table(data);
-  console.log('Total: $' + _Helpers2.default.round(total));
-}
 _commander2.default.parse(process.argv);
